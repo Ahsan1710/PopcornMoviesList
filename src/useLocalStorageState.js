6ca4fae3a20ef5ebe9ccd5d2689153b74ab0d69d
Ahsan Ahmed <ahsanahmad779@outlook.com>
2025-04-15 20:45:31 +0500

    Final Project setup

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  // Initialziang state using callback function, only callback function can initialize states not a direct function call
+  const [value, setValue] = useState(function () {
+    const watchedMovies = localStorage.getItem(key)
+      ? JSON.parse(localStorage.getItem(key))
+      : initialState;
+    return watchedMovies;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem("value", JSON.stringify(value));
+    },
+    [value]
+  );
+
+  return [value, setValue];
+}
